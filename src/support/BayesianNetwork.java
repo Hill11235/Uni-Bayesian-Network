e91f4a57cabbb4012a103107fb60588aacf27c08
eh250 <eh250@st-andrews.ac.uk>
2022-03-17 14:55:20 +0000

    Added constructor and some TODO comments to BayesianNetwork class.

--- src/support/BayesianNetwork.java
+++ src/support/BayesianNetwork.java
@@ -2,4 +2,12 @@ package support;
 
 //TODO implement bn class using guidance in the notes
 public class BayesianNetwork {
+
+    public BayesianNetwork() {
+
+    }
+    //list of Nodes
+    //method to add Nodes
+
+    //print network method
 }
