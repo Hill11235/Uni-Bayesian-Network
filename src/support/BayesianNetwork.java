630511537dc7b581c61174e986fde0c3e17441be
eh250 <eh250@st-andrews.ac.uk>
2022-03-20 11:29:00 +0000

    Added initial methods to BayesianNetwork class.

--- src/support/BayesianNetwork.java
+++ src/support/BayesianNetwork.java
@@ -1,13 +1,31 @@
 package support;
 
+import java.util.ArrayList;
+
 //TODO implement bn class using guidance in the notes
 public class BayesianNetwork {
 
-    public BayesianNetwork() {
+    private String name;
+    private ArrayList<Node> nodes = new ArrayList<>();
 
+    public BayesianNetwork(String name) {
+        this.name = name;
     }
-    //list of Nodes
-    //method to add Nodes
 
+    public void addNode(Node node) {
+        this.nodes.add(node);
+    }
+
+    public ArrayList<Node> getNodes() {
+        return nodes;
+    }
+
+    public void printNetwork() {
+        for (Node nd: nodes) {
+            System.out.println(nd.getLabel());
+            nd.printNode();
+            System.out.println();
+        }
+    }
     //print network method
 }
