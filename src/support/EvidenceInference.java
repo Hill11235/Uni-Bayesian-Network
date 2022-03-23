5ea74184a451f77a88b94409b00e83825ff3ffd6
eh250 <eh250@st-andrews.ac.uk>
2022-03-23 15:01:16 +0000

    Added EvidenceInference class for P3 and added to do comments

---
+++ src/support/EvidenceInference.java
@@ -0,0 +1,10 @@
+package support;
+
+public class EvidenceInference extends SimpleInference {
+
+    //TODO add evidence projection method to this class and superclass.
+    //TODO add evidence converter as part of above method?
+    //TODO override prune method so that it also checks against evidence ancestors.
+    //TODO override getValue method to include functionality for multiple factors and normalisation.
+
+}
