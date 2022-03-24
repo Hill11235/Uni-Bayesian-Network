52d0dea344a61c57875d6b0a6b1c0c3cbfc5f5d0
eh250 <eh250@st-andrews.ac.uk>
2022-03-24 12:31:44 +0000

    Removed star import in EvidenceInference class.

--- src/support/EvidenceInference.java
+++ src/support/EvidenceInference.java
@@ -1,6 +1,11 @@
 package support;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
