a34895c3f432613d0f24c016b72aa3611e452f02
eh250 <eh250@st-andrews.ac.uk>
2022-03-25 17:52:29 +0000

    Added javadoc to BayesianNetwork class.

--- src/support/BayesianNetwork.java
+++ src/support/BayesianNetwork.java
@@ -3,23 +3,43 @@ package support;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Class to represent Bayesian networks. Essentially collects Nodes together.
+ */
 public class BayesianNetwork {
 
-    private String name;
-    private ArrayList<Node> nodes = new ArrayList<>();
+    private final String name;
+    private final ArrayList<Node> nodes = new ArrayList<>();
 
+    /**
+     * Constructor for class.
+     * @param name the name of the network.
+     */
     public BayesianNetwork(String name) {
         this.name = name;
     }
 
+    /**
+     * Method to add Node(s).
+     * @param node Node(s) to be added to network.
+     */
     public void addNode(Node ... node) {
         this.nodes.addAll(Arrays.asList(node));
     }
 
+    /**
+     * Get Nodes in this network.
+     * @return arraylist of Nodes in the network.
+     */
     public ArrayList<Node> getNodes() {
         return nodes;
     }
 
+    /**
+     * Get Node from network based on its label.
+     * @param label label of Node to be retrieved.
+     * @return requested Node.
+     */
     public Node getNode(String label) {
         for (Node node : nodes) {
             if (node.getLabel().equals(label)) {
@@ -29,6 +49,9 @@ public class BayesianNetwork {
         return null;
     }
 
+    /**
+     * P1 requirement - print the network Node by Node.
+     */
     public void printNetwork() {
         System.out.println("Network: " + this.name);
         for (Node nd: nodes) {
